84f2b091ba2cf3e42e8a136129e650d9752ee89b
Paulo Martins <paulovelho@paulovelho.com>
2023-12-13 22:29:21 -0300

    back for 2023

--- queries.sql
+++ queries.sql
@@ -1,17 +1,22 @@
 /* GET UNIQUE countries */
-SELECT country, count(1) FROM `checkin` GROUP BY country
+SELECT country, count(1) FROM `checkin` GROUP BY country;
+
+/* GET UNIQUE countries separate by comma */
+SELECT UNIQUE country FROM checkin ORDER BY country;
+SELECT GROUP_CONCAT(DISTINCT country ORDER BY country ASC) AS countries FROM checkin;
+
 
 /* GET UNIQUE cities */
 SELECT country, city, COUNT(arrival) AS visits 
 FROM `checkin` 
 GROUP BY city
-ORDER BY visits DESC
+ORDER BY visits DESC;
 
 /* GET UNIQUE cities */
-SELECT country, city, COUNT(arrival) AS visits, SUM(DATEDIFF(departure, arrival)) AS 'days'
+SELECT country, city, COUNT(arrival) AS visits, SUM(DATEDIFF(departure, arrival)) AS 'days', SUM(lived)
 FROM `checkin`
 GROUP BY city
-ORDER BY days DESC
+ORDER BY days DESC;
 
 
 
